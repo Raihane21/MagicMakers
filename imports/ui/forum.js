b913a2ffccb42fa5bffbb43f8b9e95b4b5089b12
Thomas Buchler <thomasbuchler2005@gmail.com>
2021-03-06 13:15:35 +0100

    test

--- imports/ui/forum.js
+++ imports/ui/forum.js
@@ -5,20 +5,23 @@ import './forum.html';
 import './forum.css';
 import './poste-forum.js';
 
-Session.set('page', 'rien');   
-
-Template.forum.helpers({
-    //template dynamique
-    currentPage: function(page){
-        return Session.get('page');
-}});
-
+Session.set('page', 'rien');    
 Template.forum.events({
     //bouton information
     'click .info'(event){
         Session.set('page', 'information');
-    },
+    }});
+
+Template.forum.events({
     'click .retour'(event){
         Session.set('page', 'rien');
-    }
-});
+    }});
+    
+
+
+ Template.forum.helpers({
+
+        //template dynamique
+        currentPage: function(page){
+            return Session.get('page');
+}});
