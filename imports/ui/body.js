34d802cc25ef781892fa1ffae395acdb9c4ada30
Thomas Buchler <thomasbuchler2005@gmail.com>
2021-03-06 14:01:47 +0100

    redirections des boutons

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,3 +6,46 @@ import './forum.js';
 import "./accueil.js";
 import "./produits.js";
 import "./profil.js";
+
+
+//redirections vers les autre pages
+Session.set('site', 'forum');
+
+Template.body.events({
+    'click .aller-vers-profil'(event){
+        Session.set('site', 'profil');
+    }});
+
+Template.body.events({
+    'click .aller-vers-accueil'(event){
+        Session.set('site', 'accueil');
+    }});
+Template.body.events({
+    'click .aller-vers-accueil'(event){
+        Session.set('site', 'accueil');
+    }});
+
+Template.body.helpers({
+
+    //template dynamique
+    currentPage: function(site){
+        return Session.get('site');
+}});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
