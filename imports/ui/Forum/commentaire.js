2495202d8c131b2532e9e8983ec3d731ee5bf2d3
Thomas Buchler <thomasbuchler2005@gmail.com>
2021-05-22 14:21:21 +0200

    reponse avis 1

--- imports/ui/Forum/commentaire.js
+++ imports/ui/Forum/commentaire.js
@@ -1,13 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import './commentaire.html';
+import { Commentaires } from '../../bdd/Commentaires.js' 
 
 
 Template.commentaire.events({
-    "submit repondre" (event) {
-        var newreponses = reponses;
+    'submit .repondre'(event) {
+        event.preventDefault();
+        
+        var newreponses = this.reponses;
+        console.log(this.reponses)
         console.log("test")
         newreponses.push({ test: "truc"})
-        commentaires.update(this._id, { $set: {contact: nouveau_contact }});
-    }
+        
+        Commentaires.update(this._id, { $set: { reponses: newreponses }});
+    },
+
+    
+    'click .remove'() {
+        Commentaires.remove(this._id);
+        }
 });
+
