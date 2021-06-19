28a6e61deabc672a09d8dc15f089fd94afcf41d0
Axel BAL <corberoc@gmail.com>
2021-06-19 10:24:36 +0200

    merge ryan et ajout forum

--- imports/ui/Accueil/accueil.js
+++ imports/ui/Accueil/accueil.js
@@ -2,3 +2,22 @@ import { Template } from 'meteor/templating';
 
 import "./accueil.html";
 import "./accueil.css"; 
+import "./template.js";
+import "../Produits/produits.js"; 
+
+Session.set('menu', '');
+
+Template.accueil.events({
+    'click .afficher-le-menu'(event){
+
+        if (Session.get('menu')==='menu') {Session.set('menu','')}
+        else {Session.set('menu', 'menu')} 
+
+}});   
+
+Template.accueil.helpers({
+
+//template dynamique
+menuonoff: function(menu){
+    return Session.get('menu');
+}});
--- imports/ui/Forum/avis.js
+++ imports/ui/Forum/avis.js
@@ -3,3 +3,21 @@ import { Template } from 'meteor/templating';
 import './avis.html';
 import './forum.css';
 import { Commentaires } from '../../bdd/Commentaires.js' 
+
+Template.avis.events({
+    'submit .repondre'(event) {
+        event.preventDefault();
+        
+        var newreponses = Template.currentData().message.reponses;
+        newreponses.push({ test: event.target.reponse.value})
+        
+        Commentaires.update(Template.currentData().message._id, { $set: { reponses: newreponses }});
+    },
+    'click .removerep'(event){
+        var newreponses = Template.currentData().message.reponses;
+        newreponses.splice(event.target.getAttribute('data-value'), 1);
+        console.log(newreponses);
+
+        Commentaires.update(Template.currentData().message._id, { $set: { reponses: newreponses }});
+    },
+});
